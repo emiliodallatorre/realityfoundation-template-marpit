c8425d779be1d63355761cc70f257c860af0892c
Emilio Dalla Torre <info@emiliodallatorre.it>
2026-05-09 10:59:23 +0100

    Fixed Docker build

--- compile.sh
+++ compile.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+OUTFILE="TEMPLATE.pdf"
+
+rm -f "$OUTFILE"
+
 docker run --rm --init \
+  --user "$(id -u):$(id -g)" \
   -v "$PWD:/home/marp/app/" \
   -e "LANG=${LANG:-C.UTF-8}" \
   marpteam/marp-cli \
   TEMPLATE.md \
   --pdf \
+  --output "$OUTFILE" \
   --theme-set "./realityfoundation-bright.css" \
   --allow-local-files
